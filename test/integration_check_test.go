f72a912fbcb6af00bffd48563be34f929adce7f5
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2022-05-28 09:32:29 -0500

    Add additional test cases for check command for when there are no versions

--- test/integration_check_test.go
+++ test/integration_check_test.go
@@ -14,17 +14,22 @@ import (
 
 var _ = Describe("Integration Check", func() {
 	var (
-		inputRepo string
-		output    string
+		inputRepo         string
+		semverConstraint  string
+		includePreRelease bool = true
+
+		output []resource.Version
 	)
 
 	JustBeforeEach(func() {
 		checkRequest := resource.CheckRequest{
 			Source: resource.Source{
-				GiteaURL:    ServerURL,
-				Owner:       Username,
-				Repository:  inputRepo,
-				AccessToken: accessToken,
+				GiteaURL:          ServerURL,
+				Owner:             Username,
+				Repository:        inputRepo,
+				AccessToken:       accessToken,
+				IncludePreRelease: includePreRelease,
+				SemverConstraint:  semverConstraint,
 			},
 		}
 
@@ -37,7 +42,9 @@ var _ = Describe("Integration Check", func() {
 		cmd.Stdout = &stdout
 		Ω(err).ShouldNot(HaveOccurred())
 		Ω(cmd.Run()).To(Succeed())
-		output = strings.TrimSpace(stdout.String())
+
+		outputStr := strings.TrimSpace(stdout.String())
+		Ω(json.Unmarshal([]byte(outputStr), &output)).To(Succeed())
 	})
 
 	Context("when this is the first time that the resource has been run", func() {
@@ -47,7 +54,87 @@ var _ = Describe("Integration Check", func() {
 			})
 
 			It("returns no versions", func() {
-				Ω(output).Should(Equal("[]"))
+				Ω(output).Should(BeEmpty())
+			})
+		})
+
+		Context("when there are releases with prelease latest", func() {
+			BeforeEach(func() {
+				inputRepo = PublicRepoWithPrereleaseLatest
+			})
+
+			Context("and prerelease included", func() {
+				It("returns latest prerelease version", func() {
+					Ω(len(output)).Should(Equal(1))
+					Ω(output[0].Tag).Should(Equal("v0.0.2-alpha.1"))
+				})
+			})
+
+			Context("and prerelease not included", func() {
+				BeforeEach(func() {
+					includePreRelease = false
+				})
+
+				It("returns latest production release version", func() {
+					Ω(len(output)).Should(Equal(1))
+					Ω(output[0].Tag).Should(Equal("v0.0.1"))
+				})
+			})
+		})
+
+		Context("when there are releases with production latest", func() {
+			BeforeEach(func() {
+				inputRepo = PublicRepo
+			})
+
+			Context("and prerelease included", func() {
+				It("returns latest production release version", func() {
+					Ω(len(output)).Should(Equal(1))
+					Ω(output[0].Tag).Should(Equal("v0.0.1"))
+				})
+			})
+
+			Context("and prerelease not included", func() {
+				BeforeEach(func() {
+					includePreRelease = false
+				})
+
+				It("returns latest production release version", func() {
+					Ω(len(output)).Should(Equal(1))
+					Ω(output[0].Tag).Should(Equal("v0.0.1"))
+				})
+			})
+
+			Context("and semver constraint omit latest", func() {
+				BeforeEach(func() {
+					semverConstraint = "< v0.0.1"
+				})
+
+				It("returns latest matching", func() {
+					Ω(len(output)).Should(Equal(1))
+					Ω(output[0].Tag).Should(Equal("v0.0.0"))
+				})
+			})
+
+			Context("and semver constraint include latest", func() {
+				BeforeEach(func() {
+					semverConstraint = "< v0.0.2"
+				})
+
+				It("returns latest matching", func() {
+					Ω(len(output)).Should(Equal(1))
+					Ω(output[0].Tag).Should(Equal("v0.0.1"))
+				})
+			})
+
+			Context("and semver constraint exclude all", func() {
+				BeforeEach(func() {
+					semverConstraint = "~> v0.1.0"
+				})
+
+				It("returns no versions", func() {
+					Ω(output).Should(BeEmpty())
+				})
 			})
 		})
 	})
